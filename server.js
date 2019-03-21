1b8d3fb0c50ff99202914041710a01f8faf5a8e1
ORIENTSOFTWARE\vnakha <vonguyen.anhkha@gmail.com>
2019-03-21 19:29:02 +0700

    refactor middleware

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 var config = require('./server/config/config');
 var router = require('./server/routes/api-routes');
-var logger = require('./server/logging/logger');
 const app = express();
 
 // db.url is different depending on NODE_ENV
@@ -12,13 +11,8 @@ require('mongoose').connect(config.db.url, {
 // setup the Express middlware
 require('./server/middlewares/middleware')(app);
 
-app.use(require('morgan')({ "stream": logger.stream }));
-
 // setup the routes
 app.use("/", router);
-
-logger.info('message info');
-logger.error('message error');
 
 app.listen(config.port, () => {
   console.log('running server on port ' + config.port);
